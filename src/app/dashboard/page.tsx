5ad9b6dde368bbd95cc59f2447704b3eefaa2e52
Matheus Stein Nassr <msteinnassr@outlook.com>
2025-06-12 02:46:00 -0300

    feat: implement clinic creation form and dialog component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { db } from "@/db";
 import { auth } from "@/lib/auth";
 
 import SignOutButton from "./components/sign-out-button";
@@ -12,11 +13,33 @@ const DashboardPage = async () => {
   if (!session?.user) {
     redirect("/authentication");
   }
+
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: (usersToClinicsTable, { eq }) =>
+      eq(usersToClinicsTable.userId, session.user.id),
+    with: {
+      clinic: true,
+    },
+  });
+
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
+  }
+
   return (
-    <div>
+    <div className="p-6">
       <h1>Dashboard</h1>
-      <h1>{session?.user?.name}</h1>
-      <h1>{session?.user?.email}</h1>
+      <h1>Username: {session?.user?.name}</h1>
+      <h1>E-mail: {session?.user?.email}</h1>
+      <h2>
+        Clínica:{" "}
+        {clinics.map((clinic, idx) => (
+          <span key={clinic.clinicId}>
+            {clinic.clinic.name}
+            {idx < clinics.length - 1 && ", "}
+          </span>
+        ))}
+      </h2>
       <SignOutButton />
     </div>
   );
