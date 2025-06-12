d11e91e2e8efb7e7417d7496e6a8982c8d4ab515
Matheus Stein Nassr <msteinnassr@outlook.com>
2025-06-12 02:05:27 -0300

    feat: add sign out button and update dashboard to include user info

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,23 @@
-const DashboardPage = () => {
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="mb-4 text-4xl font-bold">Dashboard</h1>
-      <p className="text-lg">Welcome to your dashboard!</p>
+    <div>
+      <h1>Dashboard</h1>
+      <h1>{session?.user?.name}</h1>
+      <h1>{session?.user?.email}</h1>
+      <SignOutButton />
     </div>
   );
 };
